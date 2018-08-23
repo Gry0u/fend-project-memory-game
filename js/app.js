6a8a528829869ec4ee0e96febdc0fef7c758fbdc
Gauthier Riou <gauthier.riou@airbus.com>
2018-08-23 11:05:01 +0200

    feat: Add StartGame(). Sfuffle cards and add them to HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+ //Spread HTML collection into array
+let cards = [...document.getElementsByClassName('card')];
 
 /*
  * Display the cards on the page
@@ -12,7 +13,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,10 +26,24 @@ function shuffle(array) {
     return array;
 }
 
+function StartGame() {
+//1.Shuffle cards
+  cards = shuffle(cards)
 
+//2.Add each card to HTML
+//Note that with appendChild, if an element already exists it will be moved rather than duplicated!
+  for (const card of cards) {
+    document.querySelector('.deck').appendChild(card);
+  }
+}
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - display the card's symbol (put this functionality in another function that you call from this one)*/
+
+
+
+
+ /*
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
