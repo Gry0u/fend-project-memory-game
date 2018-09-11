b8a8677743f86bb190ec3bf83d841b00be322ebb
Gauthier Riou <gauthier.riou@airbus.com>
2018-09-11 17:01:22 +0200

    style: formatting

--- js/app.js
+++ js/app.js
@@ -34,10 +34,10 @@ function shuffle(array) {
 
 //Show all cards
 function showAll() {
-for (let child of deck.children) {
-  child.classList.add('open');
-  child.classList.add('show');
-  child.classList.remove('match');
+  for (let child of deck.children) {
+    child.classList.add('open');
+    child.classList.add('show');
+    child.classList.remove('match');
   }
 }
 
@@ -46,8 +46,8 @@ function hideAll() {
   for (let child of deck.children) {
     child.classList.remove('open');
     child.classList.remove('show');
-    }
   }
+}
 
 function startGame() {
 //1.Shuffle cards
@@ -80,81 +80,81 @@ function showCard(evt) {
 deck.addEventListener('click', showCard);
 
 //Function managing gameplay:
- function play(evt) {
-   //add clicked card to a list of open cards
-   openCards.push(evt.target);
-   //if the list already has another card, check if cards match
-   if (openCards.length > 0) {
-     if (openCards[0].firstElementChild.getAttribute('class') == openCards[1].firstElementChild.getAttribute('class')) {
-       match()
-       //if all cards have been matched, show winning message
-       if (checkAllMatches()) {
-         win();
-       }
-     }
-     //Otherwise hide back cards
-     else {
-       noMatch();
-     }
-     //increment counter
-     moves += 1;
-     //Update stars rating
-     updateRating(moves);
-     counter.textContent = moves;
-   }
- }
+function play(evt) {
+  //add clicked card to a list of open cards
+  openCards.push(evt.target);
+  //if the list already has another card, check if cards match
+  if (openCards.length > 0) {
+    if (openCards[0].firstElementChild.getAttribute('class') == openCards[1].firstElementChild.getAttribute('class')) {
+      match()
+      //if all cards have been matched, show winning message
+      if (checkAllMatches()) {
+        win();
+      }
+    }
+  //Otherwise hide back cards
+  else {
+    noMatch();
+  }
+  //increment counter
+  moves += 1;
+  //Update stars rating
+  updateRating(moves);
+  counter.textContent = moves;
+  }
+}
 
 //Add event listener for gameplay functionalities
- deck.addEventListener('click', play);
+deck.addEventListener('click', play);
 
 //if the cards do match, lock cards in  open position
- function match() {
-   for (let c of ['show', 'open', 'match']) {
+function match() {
+ for (let c of ['show', 'open', 'match']) {
+   openCards[0].classList.toggle(c);
+   openCards[1].classList.toggle(c);
+ }
+ openCards = [];
+}
+
+//if the cards do not match, remove the cards from the list and hide the card's symbol
+function noMatch() {
+ //allow some delay before hiding back
+ setTimeout(function() {
+   for (let c of ['show', 'open']) {
      openCards[0].classList.toggle(c);
      openCards[1].classList.toggle(c);
    }
    openCards = [];
- }
-
-//if the cards do not match, remove the cards from the list and hide the card's symbol
- function noMatch() {
-   //allow some delay before hiding back
-   setTimeout(function() {
-     for (let c of ['show', 'open']) {
-       openCards[0].classList.toggle(c);
-       openCards[1].classList.toggle(c);
-     }
-     openCards = [];
-   }, 1000);
- }
+ }, 1000);
+}
 
 //Display winning message including ellapsed time and number of moves
- function win() {
-   window.alert(`Congratulations, you completed the memory game in ${(window.performance.now() - start).toPrecision(2)/1000} seconds after ${moves} moves!`);
- }
+function win() {
+ window.alert(`Congratulations, you completed the memory game in ${(window.performance.now() - start).toPrecision(2)/1000} seconds after ${moves} moves!`);
+}
 
 //Check if all cards have been matched
- function checkAllMatches() {
-   let finish = 1;
-   for (let child of deck.children) {
-     finish *= child.classList.contains('match');
-   }
-   return finish;
+function checkAllMatches() {
+ let finish = 1;
+ for (let child of deck.children) {
+   finish *= child.classList.contains('match');
  }
+ return finish;
+}
 
 //Update number of stars based on number of moves
- function updateRating(moves) {
-   if (moves <15) {
+function updateRating(moves) {
+ if (moves <15) {
 
-   }
-   else if (moves < 25) {
-    stars[0].style.display = 'none';
-   }
-   else {
-     stars[0].style.display = 'none';
-     stars[1].style.display = 'none';
-   }
  }
+ else if (moves < 25) {
+  stars[0].style.display = 'none';
+ }
+ else {
+   stars[0].style.display = 'none';
+   stars[1].style.display = 'none';
+ }
+}
 
 //Reset game on click on corresponding button
 document.querySelector('.restart').addEventListener('click', startGame);
